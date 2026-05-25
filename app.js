ebfc360a0e67475622c0b02825ce4047533e0e1d
Juan Donday Rodríguez <jdondayr@gmail.com>
2026-05-25 10:50:48 +0000

    Elimino console.logs

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const sum = (a, b) => a + b;
 
-console.log(sum(7,3));
-
 let oneEuroIs = {
     "JPY": 156.5, // japan yen
     "USD": 1.07, // us dollar
@@ -11,7 +9,5 @@ let oneEuroIs = {
 const fromEuroToDollar = (euroAmount) => euroAmount * 1.07;
 const fromDollarToYen = (dollarAmount) => (dollarAmount / 1.07) * 156.5;
 const fromYenToPound = (yenAmount) => (yenAmount / 156.5) * 0.87;
-
-console.log(fromYenToPound(1));
 
 module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
